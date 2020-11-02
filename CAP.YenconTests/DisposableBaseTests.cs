01077aa2fd98e25e3854bdaf3c47d1db81ea3cd9
Takym <15681312+Takym@users.noreply.github.com>
2020-11-03 02:17:23 +0900

    テストプロジェクト作成

---
+++ CAP.YenconTests/DisposableBaseTests.cs
@@ -0,0 +1,42 @@
+﻿/****
+ * CAP - "Configuration and Property"
+ * Copyright (C) 2020 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAP.Tests
+{
+	[TestClass()]
+	public class DisposableBaseTests
+	{
+		[TestMethod()]
+		public void DisposeTest()
+		{
+			var obj = new DisposableBaseMock();
+			obj.Dispose();
+			Assert.IsNotNull(obj);
+			Assert.IsTrue(obj.IsDisposed);
+		}
+
+		[TestMethod()]
+		public void DisposeAsyncTest()
+		{
+			var obj = new DisposableBaseMock();
+			obj.DisposeAsync().GetAwaiter().GetResult();
+			Assert.IsNotNull(obj);
+			Assert.IsTrue(obj.IsDisposed);
+		}
+
+		private sealed class DisposableBaseMock : DisposableBase
+		{
+			protected override void Dispose(bool disposing)
+			{
+				base.Dispose(disposing);
+			}
+		}
+	}
+}
