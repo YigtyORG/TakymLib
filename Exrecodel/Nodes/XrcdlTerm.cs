23e0fc7a9ade3a2a4bff0e39ebba624ce9680e79
Takym <15681312+Takym@users.noreply.github.com>
2020-12-26 19:52:06 +0900

    用語を表す要素を作成

---
+++ Exrecodel/Nodes/XrcdlTerm.cs
@@ -0,0 +1,20 @@
+﻿/****
+ * Exrecodel - "Extensible Regulation/Convention Descriptor Language"
+ *    「拡張可能な規則/規約記述言語」
+ * Copyright (C) 2020 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+namespace Exrecodel.Nodes
+{
+	/// <summary>
+	///  用語を定義します。
+	///  このクラスは抽象クラスです。
+	/// </summary>
+	public abstract class XrcdlTerm : XrcdlNode
+	{
+		internal XrcdlTerm(XrcdlDocument document, XrcdlTermList parent) : base(document, parent) { }
+	}
+}
---
+++ Exrecodel/Nodes/XrcdlTermList.cs
@@ -0,0 +1,27 @@
+﻿/****
+ * Exrecodel - "Extensible Regulation/Convention Descriptor Language"
+ *    「拡張可能な規則/規約記述言語」
+ * Copyright (C) 2020 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System.Collections.Generic;
+
+namespace Exrecodel.Nodes
+{
+	/// <summary>
+	///  <see cref="Exrecodel.Nodes.XrcdlTerm"/>の一覧です。
+	///  このクラスは抽象クラスです。
+	/// </summary>
+	public abstract class XrcdlTermList : XrcdlNode
+	{
+		/// <summary>
+		///  この要素に格納されている全ての用語を読み取り専用で取得します。
+		/// </summary>
+		public abstract IReadOnlyList<XrcdlTerm> Children { get; }
+
+		internal XrcdlTermList(XrcdlDocument document, XrcdlNode parent) : base(document, parent) { }
+	}
+}
