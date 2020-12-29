11ea60196fdd8315134b16e0b695b4044d87eecf
Takym <15681312+Takym@users.noreply.github.com>
2020-12-30 00:46:23 +0900

    コマンド行引数解析クラス実装

---
+++ TakymLib.CommandLine/CommandLineParser.cs
@@ -0,0 +1,273 @@
+﻿/****
+ * TakymLib
+ * Copyright (C) 2020 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TakymLib.CommandLine
+{
+	/// <summary>
+	///  コマンド行引数を解析します。
+	/// </summary>
+	public class CommandLineParser
+	{
+		private readonly string[] _args;
+
+		/// <summary>
+		///  型'<see cref="TakymLib.CommandLine.CommandLineParser"/>'の新しいインスタンスを生成します。
+		/// </summary>
+		/// <param name="args">コマンド行引数です。</param>
+		/// <exception cref="System.ArgumentNullException"/>
+		public CommandLineParser(string[] args)
+		{
+			args.EnsureNotNull(nameof(args));
+			_args = args;
+		}
+
+		/// <summary>
+		///  コマンド行引数の解析を実行します。
+		/// </summary>
+		public void Parse()
+		{
+			this.ParseCore(ParseCore(new ArrayAsyncEnumerator(_args), true).ConfigureAwait(false).GetAwaiter().GetResult());
+		}
+
+		/// <summary>
+		///  コマンド行引数の解析を非同期的に実行します。
+		/// </summary>
+		/// <returns>
+		///  この処理の非同期操作です。
+		/// </returns>
+		public async Task ParseAsync()
+		{
+			this.ParseCore(await ParseCore(new ArrayAsyncEnumerator(_args), true).ConfigureAwait(false));
+		}
+
+		private static async Task<ParseResult> ParseCore(IAsyncEnumerable<string> args, bool readCommand)
+		{
+			var result = new ParseResult();
+			await using (var e = args.GetAsyncEnumerator()) {
+				if (readCommand && await e.MoveNextAsync()) {
+					string cmd = e.Current.Trim().ToLower();
+					if (cmd == "/?"    || cmd == "-?"    || cmd == "--?"    || cmd == "?" ||
+						cmd == "/help" || cmd == "-help" || cmd == "--help" ||
+						cmd == "/h"    || cmd == "-h"    || cmd == "--h") {
+						result.Command = "help";
+					} else {
+						result.Command = cmd;
+					}
+				}
+				while (await e.MoveNextAsync()) {
+					string arg = e.Current.Trim();
+					if (!string.IsNullOrEmpty(arg)) {
+						string argData = arg.Substring(1);
+						switch (arg[0]) { // argType = arg[0]
+						case '/':
+							result.AddSwitch(argData);
+							break;
+						case '-':
+							result.AddOption(argData);
+							break;
+						case '@':
+							if (File.Exists(argData)) {
+								async IAsyncEnumerable<string> LoadFile(string fname)
+								{
+									using (var fs = new FileStream(fname, FileMode.Open, FileAccess.Read, FileShare.Read))
+									using (var sr = new StreamReader(fs, true)) {
+										while (await sr.ReadLineAsync() is string line) {
+											yield return line;
+										}
+									}
+								}
+								result.Combine(await ParseCore(LoadFile(argData), false));
+							}
+							break;
+						case '#':
+							break;
+						default:
+							result.AddValue(arg);
+							break;
+						}
+					}
+				}
+			}
+			return result;
+		}
+
+		private void ParseCore(ParseResult pr)
+		{
+			this.OnPreParse();
+			{
+				int count = pr.Values.Count;
+				for (int i = 0; i < count; ++i) {
+					this.OnParse(null, null, pr.Values[i]);
+				}
+			}
+			{
+				int count = pr.Options.Count;
+				for (int i = 0; i < count; ++i) {
+					string name   = pr.Options[i].Name;
+					var    vals   = pr.Options[i].Values;
+					int    count2 = vals.Count;
+					for (int j = 0; j < count2; ++j) {
+						this.OnParse(null, name, vals[j]);
+					}
+				}
+			}
+			{
+				int count = pr.Switches.Count;
+				for (int i = 0; i < count; ++i) {
+					string name_s = pr.Switches[i].Name;
+					{
+						var vals   = pr.Switches[i].Values;
+						int count2 = vals.Count;
+						for (int j = 0; j < count2; ++j) {
+							this.OnParse(name_s, null, vals[j]);
+						}
+					}
+					{
+						var opts   = pr.Switches[i].Options;
+						int count2 = opts.Count;
+						for (int j = 0; j < count2; ++j) {
+							string name_o = opts[j].Name;
+							var    vals   = opts[j].Values;
+							int    count3 = vals.Count;
+							for (int k = 0; k < count3; ++k) {
+								this.OnParse(name_s, name_o, vals[k]);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		///  解析前に呼び出されます。
+		/// </summary>
+		protected virtual void OnPreParse() { }
+
+		/// <summary>
+		///  解析時に呼び出されます。
+		/// </summary>
+		/// <param name="switchName">スイッチ名です。</param>
+		/// <param name="optionName">オプション名です。</param>
+		/// <param name="value">値です。</param>
+		protected virtual void OnParse(string? switchName, string? optionName, string value) { }
+
+		private sealed class ParseResult
+		{
+			private  List<string> _current_vs;
+			private  List<Option> _current_os;
+			internal string?      Command  { get; set; }
+			internal List<string> Values   { get; }
+			internal List<Option> Options  { get; }
+			internal List<Switch> Switches { get; }
+
+			internal ParseResult()
+			{
+				this.Values   = new List<string>();
+				this.Options  = new List<Option>();
+				this.Switches = new List<Switch>();
+				_current_vs   = this.Values;
+				_current_os   = this.Options;
+			}
+
+			internal void AddValue(string value)
+			{
+				_current_vs.Add(value);
+			}
+
+			internal void AddSwitch(string name)
+			{
+				var s = new Switch(name);
+				_current_vs = s.Values;
+				_current_os = s.Options;
+				this.Switches.Add(s);
+			}
+
+			internal void AddOption(string name)
+			{
+				var o = new Option(name);
+				_current_vs = o.Values;
+				_current_os.Add(o);
+			}
+
+			internal void Combine(ParseResult parseResult)
+			{
+				this.Values  .AddRange(parseResult.Values);
+				this.Options .AddRange(parseResult.Options);
+				this.Switches.AddRange(parseResult.Switches);
+			}
+		}
+
+		private sealed class Switch
+		{
+			internal string       Name    { get; }
+			internal List<string> Values  { get; }
+			internal List<Option> Options { get; }
+
+			internal Switch(string name)
+			{
+				this.Name    = name;
+				this.Values  = new List<string>();
+				this.Options = new List<Option>();
+			}
+		}
+
+		private sealed class Option
+		{
+			internal string       Name   { get; }
+			internal List<string> Values { get; }
+
+			internal Option(string name)
+			{
+				this.Name   = name;
+				this.Values = new List<string>();
+			}
+		}
+
+		private struct ArrayAsyncEnumerator : IAsyncEnumerable<string>, IAsyncEnumerator<string>
+		{
+			private readonly int      _mtid;
+			private readonly string[] _array;
+			private          int      _index;
+
+			public string Current => _array[_index];
+
+			internal ArrayAsyncEnumerator(string[] array)
+			{
+				_mtid  = Thread.CurrentThread.ManagedThreadId;
+				_array = array;
+				_index = -1;
+			}
+
+			public ValueTask<bool> MoveNextAsync()
+			{
+				++_index;
+				return new ValueTask<bool>(_index < _array.Length);
+			}
+
+			public IAsyncEnumerator<string> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+			{
+				if (Thread.CurrentThread.ManagedThreadId == _mtid) {
+					return this;
+				} else {
+					throw new InvalidOperationException();
+				}
+			}
+
+			public ValueTask DisposeAsync()
+			{
+				return default;
+			}
+		}
+	}
+}
---
+++ TakymLib.CommandLine/SwitchAttribute.cs
@@ -0,0 +1,38 @@
+﻿/****
+ * TakymLib
+ * Copyright (C) 2020 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System;
+
+namespace TakymLib.CommandLine
+{
+	/// <summary>
+	///  コマンド行引数からの情報を保持するクラスまたは構造体である事を示します。
+	///  このクラスは継承できません。
+	/// </summary>
+	/// <remarks>
+	///  クラスまたは構造体の引数無しコンストラクタは公開されている必要があります。
+	///  また、読み取り専用構造体と参照構造体は利用できません。
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+	public sealed class SwitchAttribute : Attribute
+	{
+		/// <summary>
+		///  名前を取得します。
+		/// </summary>
+		public string? Name { get; }
+
+		/// <summary>
+		///  型'<see cref="TakymLib.CommandLine.SwitchAttribute"/>'の新しいインスタンスを生成します。
+		/// </summary>
+		/// <param name="longName">名前を設定します。</param>
+		public SwitchAttribute(string? longName)
+		{
+			this.Name = longName;
+		}
+	}
+}
