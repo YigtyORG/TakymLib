0762cac4ddd65d71554a75a458a73b79a2f80178
Takym <15681312+Takym@users.noreply.github.com>
2021-01-07 16:35:44 +0900

    LogFileName、テスト処理実装

---
+++ TakymLibTests/TakymLib/Logging/LogFileNameTests.cs
@@ -0,0 +1,32 @@
+﻿/****
+ * TakymLib
+ * Copyright (C) 2020-2021 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020-2021 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TakymLib.IO;
+using TakymLib.Logging;
+
+namespace TakymLibTests.TakymLib.Logging
+{
+	[TestClass()]
+	public class LogFileNameTests
+	{
+		[TestMethod()]
+		public void CreateTest()
+		{
+			Assert.AreEqual(
+				"000101010000000000000.[0].log",
+				LogFileName.Create(new DateTime(0), null)
+			);
+			Assert.AreEqual(
+				"0001-01-01_00-00-00+0000000.[0].log",
+				LogFileName.Create(new DateTime(0), null, null, true)
+			);
+		}
+	}
+}
