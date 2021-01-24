7a66de20bdb78a68c955972cbf9a2260e6092612
Takym <15681312+Takym@users.noreply.github.com>
2021-01-25 05:54:29 +0900

    XML文書コメントの誤りを修正。

--- TakymLib/DisposableBase.cs
+++ TakymLib/DisposableBase.cs
@@ -60,7 +60,7 @@ namespace TakymLib
 		/// <summary>
 		///  現在のオブジェクトインスタンスと利用しているリソースを非同期で破棄します。
 		/// </summary>
-		/// <remarks>この処理の非同期操作です。</remarks>
+		/// <returns>この処理の非同期操作です。</returns>
 		public async ValueTask DisposeAsync()
 		{
 			this.IsDisposing = true;
@@ -87,7 +87,7 @@ namespace TakymLib
 		///  現在のオブジェクトインスタンスと利用しているリソースを非同期で破棄します。
 		///  この関数内で例外を発生させてはいけません。
 		/// </summary>
-		/// <remarks>この処理の非同期操作です。</remarks>
+		/// <returns>この処理の非同期操作です。</returns>
 		protected virtual ValueTask DisposeAsyncCore()
 		{
 			return default;
