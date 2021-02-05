1dee130a2cc5ec326a3edbc32a2310cdca345404
Takym <15681312+Takym@users.noreply.github.com>
2021-02-06 01:15:16 +0900

    ICallerLogger実装

---
+++ TakymLib.Logging/ICallerLogger.cs
@@ -0,0 +1,108 @@
+﻿/****
+ * TakymLib
+ * Copyright (C) 2020-2021 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020-2021 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System.Runtime.CompilerServices;
+
+namespace TakymLib.Logging
+{
+	/// <summary>
+	///  呼び出し元の情報をログ出力する機能を提供します。
+	/// </summary>
+	public interface ICallerLogger
+	{
+		/// <summary>
+		///  処理の開始をログに出力します。
+		/// </summary>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		public void Begin(
+			[CallerMemberName()] string memberName = "",
+			[CallerFilePath()]   string filePath   = "",
+			[CallerLineNumber()] int    lineNumber = -1
+		);
+
+		/// <summary>
+		///  処理の開始をログに出力します。
+		/// </summary>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		public void Begin(
+			                                      object? message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = ""
+		);
+
+		/// <summary>
+		///  処理の開始をログに出力します。
+		/// </summary>
+		/// <typeparam name="T">メッセージの種類です。</typeparam>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		public void Begin<T>(
+			                                      T?      message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = ""
+		);
+
+		/// <summary>
+		///  処理の終了をログに出力します。
+		/// </summary>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		public void End(
+			[CallerMemberName()] string memberName = "",
+			[CallerFilePath()]   string filePath   = "",
+			[CallerLineNumber()] int    lineNumber = -1
+		);
+
+		/// <summary>
+		///  処理の終了をログに出力します。
+		/// </summary>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		public void End(
+			                                      object? message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = ""
+		);
+
+		/// <summary>
+		///  処理の終了をログに出力します。
+		/// </summary>
+		/// <typeparam name="T">メッセージの種類です。</typeparam>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		public void End<T>(
+			                                      T?      message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = ""
+		);
+	}
+}
---
+++ TakymLib.Logging/TextWriterCallerLogger.cs
@@ -0,0 +1,191 @@
+﻿/****
+ * TakymLib
+ * Copyright (C) 2020-2021 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020-2021 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TakymLib.Logging
+{
+	/// <summary>
+	///  呼び出し元の情報を出力ストリームに書き込む機能を提供します。
+	/// </summary>
+	public class TextWriterCallerLogger : DisposableBase, ICallerLogger
+	{
+		private static          TextWriterCallerLogger? _stdout;
+		private        readonly TextWriter              _tw;
+
+		/// <summary>
+		///  標準出力ストリームに出力を行うロガーを取得します。
+		/// </summary>
+		public static TextWriterCallerLogger StandardOutput
+		{
+			get
+			{
+				if (_stdout is null || _stdout.IsDisposing || _stdout.IsDisposed) {
+					_stdout = new(Console.Out);
+				}
+				return _stdout;
+			}
+		}
+
+		/// <summary>
+		///  型'<see cref="TakymLib.Logging.TextWriterCallerLogger"/>'の新しいインスタンスを生成します。
+		/// </summary>
+		/// <param name="tw">出力先のライターです。</param>
+		public TextWriterCallerLogger(TextWriter tw)
+		{
+			tw.EnsureNotNull(nameof(tw));
+			_tw = tw;
+		}
+
+		/// <summary>
+		///  処理の開始をログに出力します。
+		/// </summary>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <exception cref="System.ObjectDisposedException"/>
+		public virtual void Begin(
+			[CallerMemberName()] string memberName = "",
+			[CallerFilePath()]   string filePath   = "",
+			[CallerLineNumber()] int    lineNumber = -1)
+		{
+			this.EnsureNotDisposed();
+			_tw.WriteLine($"[{DateTime.Now:yyyy/MM/dd HH:mm:ss.fffffff}] begin {memberName} `{filePath}({lineNumber})`");
+		}
+
+		/// <summary>
+		///  処理の開始をログに出力します。
+		/// </summary>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		/// <exception cref="System.ObjectDisposedException"/>
+		public virtual void Begin(
+			                                      object? message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = "")
+		{
+			this.EnsureNotDisposed();
+			_tw.WriteLine($"[{DateTime.Now:yyyy/MM/dd HH:mm:ss.fffffff}] begin {memberName}; {messageExpression} `{filePath}({lineNumber})`");
+		}
+
+		/// <summary>
+		///  処理の開始をログに出力します。
+		/// </summary>
+		/// <typeparam name="T">メッセージの種類です。</typeparam>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		/// <exception cref="System.ObjectDisposedException"/>
+		public virtual void Begin<T>(
+			                                      T?      message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = "")
+		{
+			this.EnsureNotDisposed();
+			_tw.WriteLine($"[{DateTime.Now:yyyy/MM/dd HH:mm:ss.fffffff}] begin {memberName}; {messageExpression} `{filePath}({lineNumber})`");
+		}
+
+		/// <summary>
+		///  処理の終了をログに出力します。
+		/// </summary>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <exception cref="System.ObjectDisposedException"/>
+		public virtual void End(
+			[CallerMemberName()] string memberName = "",
+			[CallerFilePath()]   string filePath   = "",
+			[CallerLineNumber()] int    lineNumber = -1)
+		{
+			this.EnsureNotDisposed();
+			_tw.WriteLine($"[{DateTime.Now:yyyy/MM/dd HH:mm:ss.fffffff}] end {memberName} `{filePath}({lineNumber})`");
+		}
+
+		/// <summary>
+		///  処理の終了をログに出力します。
+		/// </summary>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		/// <exception cref="System.ObjectDisposedException"/>
+		public virtual void End(
+			                                      object? message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = "")
+		{
+			this.EnsureNotDisposed();
+			_tw.WriteLine($"[{DateTime.Now:yyyy/MM/dd HH:mm:ss.fffffff}] end {memberName}; {messageExpression} `{filePath}({lineNumber})`");
+		}
+
+		/// <summary>
+		///  処理の終了をログに出力します。
+		/// </summary>
+		/// <typeparam name="T">メッセージの種類です。</typeparam>
+		/// <param name="message">出力するメッセージです。</param>
+		/// <param name="memberName">呼び出し元の名前です。</param>
+		/// <param name="filePath">呼び出し元のコードを格納しているファイルのパスです。</param>
+		/// <param name="lineNumber">呼び出し元のコードの行番号です。</param>
+		/// <param name="messageExpression">メッセージの式表現です。</param>
+		/// <exception cref="System.ObjectDisposedException"/>
+		public virtual void End<T>(
+			                                      T?      message,
+			[CallerMemberName()]                  string  memberName        = "",
+			[CallerFilePath()]                    string  filePath          = "",
+			[CallerLineNumber()]                  int     lineNumber        = -1,
+			[CallerArgumentExpression("message")] string  messageExpression = "")
+		{
+			this.EnsureNotDisposed();
+			_tw.WriteLine($"[{DateTime.Now:yyyy/MM/dd HH:mm:ss.fffffff}] end {memberName}; {messageExpression} `{filePath}({lineNumber})`");
+		}
+
+		/// <summary>
+		///  現在のオブジェクトインスタンスと利用しているリソースを破棄します。
+		///  この関数内で例外を発生させてはいけません。
+		/// </summary>
+		/// <param name="disposing">
+		///  マネージドオブジェクトとアンマネージオブジェクト両方を破棄する場合は<see langword="true"/>、
+		///  アンマネージオブジェクトのみを破棄する場合は<see langword="false"/>を設定します。
+		/// </param>
+		protected override void Dispose(bool disposing)
+		{
+			if (!this.IsDisposed) {
+				if (disposing) {
+					_tw.Dispose();
+				}
+				base.Dispose(disposing);
+			}
+		}
+
+		/// <summary>
+		///  現在のオブジェクトインスタンスと利用しているリソースを非同期で破棄します。
+		///  この関数内で例外を発生させてはいけません。
+		/// </summary>
+		/// <returns>この処理の非同期操作です。</returns>
+		protected override async ValueTask DisposeAsyncCore()
+		{
+			await _tw.ConfigureAwait(false).DisposeAsync();
+			await base.DisposeAsyncCore();
+		}
+	}
+}
