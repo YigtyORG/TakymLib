7a26e7c85c2d1ed43a5545aae2bd1a97d8a6bc31
Takym <15681312+Takym@users.noreply.github.com>
2021-03-31 23:04:15 +0900

    EastAsianWidth、v4.1～v12.1対応

--- TakymLib.ConsoleApp/Program.cs
+++ TakymLib.ConsoleApp/Program.cs
@@ -7,6 +7,8 @@
 ****/
 
 using System;
+using System.IO;
+using System.Net;
 using System.Text;
 using TakymLib.Text;
 
@@ -16,7 +18,54 @@ namespace TakymLib.ConsoleApp
 	{
 		private static void Run()
 		{
-			DownloadLatestDefinitionAndConvert();
+			//DownloadLatestDefinitionAndConvert();
+			using (var wc = new WebClient()) {
+				DownloadEAWDefinitionAndConvert("4.1.0", wc);
+				DownloadEAWDefinitionAndConvert("5.0.0", wc);
+				DownloadEAWDefinitionAndConvert("5.1.0", wc);
+				DownloadEAWDefinitionAndConvert("5.2.0", wc);
+				DownloadEAWDefinitionAndConvert("6.0.0", wc);
+				DownloadEAWDefinitionAndConvert("6.1.0", wc);
+				DownloadEAWDefinitionAndConvert("6.2.0", wc);
+				DownloadEAWDefinitionAndConvert("6.3.0", wc);
+				DownloadEAWDefinitionAndConvert("7.0.0", wc);
+				DownloadEAWDefinitionAndConvert("8.0.0", wc);
+				DownloadEAWDefinitionAndConvert("9.0.0", wc);
+				DownloadEAWDefinitionAndConvert("10.0.0", wc);
+				DownloadEAWDefinitionAndConvert("11.0.0", wc);
+				DownloadEAWDefinitionAndConvert("12.0.0", wc);
+				DownloadEAWDefinitionAndConvert("12.1.0", wc);
+				DownloadEAWDefinitionAndConvert("13.0.0", wc);
+			}
+		}
+
+		private static void DownloadEAWDefinitionAndConvert(string version, WebClient wc)
+		{
+			var ranges = EastAsianWidth.DownloadDefinitionFrom($"https://www.unicode.org/Public/{version}/ucd/EastAsianWidth.txt", wc).Ranges;
+			int count  = ranges.Count;
+			using (var fs = new FileStream(version + ".txt", FileMode.Create, FileAccess.Write, FileShare.None))
+			using (var sw = new StreamWriter(fs, Encoding.UTF8)) {
+				for (int i = 0; i < count; ++i) {
+					var range = ranges[i];
+					if (range.Type == EastAsianWidthType.Invalid) {
+						continue;
+					}
+					if (range.Start > ushort.MaxValue || range.End > ushort.MaxValue) {
+						continue;
+					}
+					if (range.Start == range.End) {
+						sw.Write("\'\\u{0:X04}\'", range.Start);
+					} else {
+						sw.Write(">= \'\\u{0:X04}\' and <= \'\\u{1:X04}\'", range.Start, range.End);
+					}
+					sw.Write(" => ");
+					sw.Write(nameof(EastAsianWidthType));
+					sw.Write('.');
+					sw.Write(range.Type);
+					sw.WriteLine(",");
+				}
+			}
+			Console.WriteLine(version);
 		}
 
 		private static void DownloadLatestDefinitionAndConvert()
