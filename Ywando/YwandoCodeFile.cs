9bcc428e9be47e5a8728cb10c93ed82b45716230
Takym <15681312+Takym@users.noreply.github.com>
2021-04-17 00:08:39 +0900

    YwandoCodeFile クラス作成(空)

---
+++ Ywando/YwandoCodeFile.cs
@@ -0,0 +1,20 @@
+/****
+ * Ywando
+ * Copyright (C) 2020-2021 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020-2021 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System.Runtime.Serialization;
+
+namespace Ywando
+{
+	/// <summary>
+	///  <see langword="Ywando"/>のコードファイルを表します。
+	/// </summary>
+	[DataContract(Namespace = nameof(Ywando) + ".xsd", IsReference = true)]
+	public class YwandoCodeFile
+	{
+	}
+}
