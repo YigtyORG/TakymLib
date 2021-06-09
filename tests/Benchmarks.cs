49c82500c73423a9a1226b9ff53d80600383b251
Takym <15681312+Takym@users.noreply.github.com>
2021-06-09 22:06:37 +0900

    ベンチマーク用のビルド構成を作成

--- tests/Benchmarks.cs
+++ tests/Benchmarks.cs
@@ -8,7 +8,9 @@
 
 using System;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 #if RELEASE
@@ -22,13 +24,18 @@ namespace Exyzer.Tests
 	[DisassemblyDiagnoser()]
 	public class Benchmarks
 	{
+		private static readonly IConfig _config = DefaultConfig.Instance.AddJob(new Job()
+			.Freeze()
+			.WithCustomBuildConfiguration("Benchmark")
+		);
+
 		[TestMethod()]
 		public void Run()
 		{
-#if RELEASE
+#if BENCHMARK
 			BenchmarkSwitcher.FromTypes(new[] { this.GetType() }).RunAllJoined();
 #else
-			Console.WriteLine(nameof(Benchmarks) + " are only supported in the release build.");
+			Console.WriteLine(nameof(Benchmarks) + " are only supported in the benchmark build.");
 #endif
 		}
 	}
