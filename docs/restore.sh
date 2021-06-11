3618f5aa62ebf9c2499397001daf8ee419820115
Takym <15681312+Takym@users.noreply.github.com>
2021-06-12 07:01:35 +0900

    ライセンス通知の種類を追加

--- docs/restore.sh
+++ docs/restore.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# ***********************************************************
+# * TakymLib                                                *
+# * Copyright (C) 2020-2021 Yigty.ORG; all rights reserved. *
+# * Copyright (C) 2020-2021 Takym.                          *
+# *                                                         *
+# * distributed under the MIT License.                      *
+# ***********************************************************
+
 echo begin: restore.sh
 dotnet restore --packages=./packages
 echo end: restore.sh
