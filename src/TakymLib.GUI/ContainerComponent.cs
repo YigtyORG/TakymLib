c78112b9089424bac0d51202d51331bf01995269
Takym <15681312+Takym@users.noreply.github.com>
2021-06-18 22:31:29 +0900

    `TakymLib.GUI` の基本クラスを実装

---
+++ src/TakymLib.GUI/ContainerComponent.cs
@@ -0,0 +1,50 @@
+/****
+ * TakymLib
+ * Copyright (C) 2020-2021 Yigty.ORG; all rights reserved.
+ * Copyright (C) 2020-2021 Takym.
+ *
+ * distributed under the MIT License.
+****/
+
+using System.ComponentModel;
+
+namespace TakymLib.GUI
+{
+	/// <summary>
+	///  他の部品を格納する事ができる部品を表します。
+	/// </summary>
+	public class ContainerComponent : Component
+	{
+		/// <summary>
+		///  コンテナを取得します。
+		/// </summary>
+		public Container Container { get; }
+
+		/// <summary>
+		///  型'<see cref="TakymLib.GUI.ContainerComponent"/>'の新しいインスタンスを生成します。
+		/// </summary>
+		public ContainerComponent()
+		{
+			this.Container = new();
+		}
+
+		/// <summary>
+		///  型'<see cref="TakymLib.GUI.ContainerComponent"/>'の新しいインスタンスを生成します。
+		/// </summary>
+		/// <param name="container">新しく生成する部品を格納するオブジェクトを指定します。</param>
+		public ContainerComponent(IContainer container) : base(container)
+		{
+			this.Container = new();
+		}
+
+		/// <inheritdoc/>
+		protected override void Dispose(bool disposing)
+		{
+			if (this.IsDisposed) {
+				return;
+			}
+			this.Container.Dispose();
+			base.Dispose(disposing);
+		}
+	}
+}
--- src/TakymLib/DisposableBase.cs
+++ src/TakymLib/DisposableBase.cs
@@ -341,6 +341,26 @@ namespace TakymLib
 		///  指定された処理を安全かつ非同期的に実行します。
 		/// </summary>
 		/// <typeparam name="TState">引数の型を指定します。</typeparam>
+		/// <param name="action">実行する処理を指定します。</param>
+		/// <param name="state">処理に渡す引数を指定します。</param>
+		/// <returns>この処理の非同期操作を返します。</returns>
+		/// <exception cref="System.ObjectDisposedException"/>
+		/// <exception cref="System.InvalidOperationException"/>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected async ValueTask RunSafelyAsync<TState>(Func<TState, ValueTask> action, TState state)
+		{
+			this.EnterRunLock();
+			try {
+				await action(state);
+			} finally {
+				await this.LeaveRunLockAsync();
+			}
+		}
+
+		/// <summary>
+		///  指定された処理を安全かつ非同期的に実行します。
+		/// </summary>
+		/// <typeparam name="TState">引数の型を指定します。</typeparam>
 		/// <typeparam name="TResult">戻り値の型を指定します。</typeparam>
 		/// <param name="action">実行する処理を指定します。</param>
 		/// <param name="state">処理に渡す引数を指定します。</param>
