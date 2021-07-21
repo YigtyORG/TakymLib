1b71697fc52878dea22aa8037e8301b2e11ef403
Takym <15681312+Takym@users.noreply.github.com>
2021-07-21 19:56:32 +0900

    SimpleLocker のテスト処理を追加

--- tests/TakymLib/Threading/SimpleLockerTests.cs
+++ tests/TakymLib/Threading/SimpleLockerTests.cs
@@ -15,8 +15,10 @@ namespace TakymLibTests.TakymLib.Threading
 	[TestClass()]
 	public class SimpleLockerTests
 	{
+		#region Test #1
+
 		[TestMethod()]
-		public void Test()
+		public void Test1()
 		{
 			var  locker = new SimpleLocker();
 			bool taken  = false;
@@ -31,11 +33,11 @@ namespace TakymLibTests.TakymLib.Threading
 		}
 
 		[TestMethod()]
-		public void AsyncTest()
+		public void AsyncTest1()
 		{
-			AsyncTestCore().ConfigureAwait(false).GetAwaiter().GetResult();
+			AsyncTest1Core().ConfigureAwait(false).GetAwaiter().GetResult();
 
-			static async Task AsyncTestCore()
+			static async Task AsyncTest1Core()
 			{
 				var locker = new SimpleLocker();
 
@@ -47,5 +49,74 @@ namespace TakymLibTests.TakymLib.Threading
 				Assert.IsTrue(await locker.LeaveLockAsync());
 			}
 		}
+
+		#endregion
+
+		#region Test #2
+
+		[TestMethod()]
+		public void Test2()
+		{
+			var  locker = new SimpleLocker();
+			bool taken  = false;
+			int  value  = 0;
+
+			Test2Core().ConfigureAwait(false).GetAwaiter().GetResult();
+
+			Assert.AreEqual(32, value);
+
+			Task Test2Core()
+			{
+				var tasks = new Task[32];
+				for (int i = 0; i < tasks.Length; ++i) {
+					tasks[i] = Task.Run(Run);
+				}
+				return Task.WhenAll(tasks);
+			}
+
+			void Run()
+			{
+				try {
+					locker.EnterLock(ref taken);
+					++value;
+				} finally {
+					if (taken) {
+						locker.LeaveLock();
+					}
+				}
+			}
+		}
+
+		[TestMethod()]
+		public void AsyncTest2()
+		{
+			var locker = new SimpleLocker();
+			int value  = 0;
+
+			AsyncTest2Core().ConfigureAwait(false).GetAwaiter().GetResult();
+
+			Assert.AreEqual(32, value);
+
+			Task AsyncTest2Core()
+			{
+				var tasks = new Task[32];
+				for (int i = 0; i < tasks.Length; ++i) {
+					tasks[i] = Task.Run(RunAsync);
+				}
+				return Task.WhenAll(tasks);
+			}
+
+			async ValueTask RunAsync()
+			{
+				try {
+					await locker.EnterLockAsync();
+					++value;
+				} finally {
+					await locker.LeaveLockAsync();
+				}
+			}
+		}
+
+		#endregion
 	}
 }
